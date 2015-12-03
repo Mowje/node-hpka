dfd9fcc8544be516423e18e56613096710e06063
Ahmad Ben Mrad <batikhsouri@gmail.com>
2015-12-03 23:59:45 +0100

    Working on the client

--- test/testClient.js
+++ test/testClient.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var assert = require('assert');
 var hpka = require('../hpka');
+var crypto = require('crypto');
 var cryptopp, sodium;
 var Buffer = require('buffer').Buffer;
 var FormData = require('form-data');
@@ -12,6 +13,10 @@ var FormData = require('form-data');
 var testUsername = 'test';
 var testPassword = 'password';
 var keyType;
+
+var testClient;
+
+var serverSettings;
 
 //The options for each keyType test
 var testKeyOptions = {
@@ -76,6 +81,20 @@ function processRes(res, cb){
 	res.on('data', function(part){b += part});
 }
 
+function validStatusCode(n){
+	if (!n) return;
+	var v = typeof n == 'number' && Math.floor(n) == n && n >= 100 && n < 600;
+	if (!v) throw new TypeError('when defined, _expectedStatusCode must be an integer number, with the [100..600[ range');
+}
+
+function isString(s){
+	return typeof s == 'string' && s.length > 0;
+}
+
+function isFunction(f){
+	return typeof f == 'function';
+}
+
 exports.setKeyType = function(_keyType){
 	if (typeof _keyType != 'string') throw new TypeError('_keyType must be a string');
 
@@ -92,29 +111,106 @@ exports.setKeyType = function(_keyType){
 	if (!availKeyType) throw new Error(_keyType + ' is not supported (sodium ro cryptopp is missing)');
 };
 
-exports.setup = function(keyPath){
-	var retVal = hpka.createClientKey(keyType, testKeyOptions[keyType], keyPath, password, true);
-	
+exports.setServerSettings = function(_serverSettings){
+	if (typeof _serverSettings != 'object') throw new TypeError('_serverSettings must be an object');
+
+	serverSettings = _serverSettings;
+};
+
+exports.setup = function(keyPath, altKeyPath, allowGetSessions){
+	hpka.createClientKey(keyType, testKeyOptions[keyType], keyPath, password);
+	if (altKeyPath) hpka.createClientKey(keyPath, testKeyOptions[keyType], altKeyPath, password);
+
+	testClient = new hpka.client(keyPath, testUsername, keyType == 'ed25519' ? password : undefined, allowGetSessions);
 };
 
 //Write the different test cases that must be executed for each key type. Will be called from index.js
 exports.unauthenticatedReq = function(cb){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
 
+	performReq(serverSettings, function(err, body, res){
+		if (err) throw err;
+		assert.equal(res.statusCode, 200, 'On successful anonymous requests, status code must be 200');
+		assert.equal(body, 'Anonymous user', 'Unexpected string from server: ' + data);
+		cb();
+	});
 };
 
-exports.registrationReq = function(cb){
+exports.registrationReq = function(cb, _expectedBody, _expectedStatusCode){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
 
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
+
+	var expectedBody = _expectedBody || ('Welcome ' + testUsername + ' !');
+	var expectedStatusCode = _expectedStatusCode || 200;
+
+	testClient.registerUser(serverSettings, function(res){
+		processRes(res, function(body){
+			assert.equal(res.statusCode, expectedStatusCode, 'Unexpected status code on registration: ' + res.statusCode);
+			assert.equal(body, expectedBody, 'Unexpected message on registration: ' + body);
+			cb();
+		});
+	}, function(err){throw err;});
 };
 
-exports.authenticatedReq = function(cb, withForm){
+exports.authenticatedReq = function(cb, withForm, _expectedBody, _expectedStatusCode){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
 
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
+
+	var expectedBody = _expectedBody || ('Authenticated as : ' + testUsername);
+	var expectedStatusCode = _expectedStatusCode || 200;
+
+	testClient.request(serverSettings, undefined, function(res){
+		processRes(res, function(body){
+			assert.equal(res.statusCode, expectedStatusCode, 'Unexpected status code: ' + res.statusCode);
+			assert.equal(body, expectedBody, 'Unexpected body: ' + body);
+			cb();
+		});
+	}, function(err){throw err;});
 };
 
-exports.deletionReq = function(cb){
+exports.deletionReq = function(cb, _expectedBody, _expectedStatusCode){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
 
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
+
+	var expectedBody = _expectedBody || (testUsername + ' has been deleted!');
+	var expectedStatusCode = _expectedStatusCode || 200;
+
+	testClient.deleteUser(serverSettings, function(res){
+		processRes(res, function(body){
+			assert.equal(res.statusCode, expectedStatusCode, 'Unexpected status code on user deletion: ' + res.statusCode);
+			assert.equal(body, expectedBody, 'Unexpected response body: ' + body);
+			cb();
+		});
+	}, function(err){throw err;});
 };
 
-exports.keyRotationReq = function(cb){
+exports.keyRotationReq = function(cb, newKeyPath, _expectedBody, _expectedStatusCode){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
+
+	if (!newKeyPath) throw new TypeError('newKeyPath must be provided');
+
+	var fullKeyPath = path.join(process.cwd(), newKeyPath);
+	if (!fs.existsSync(fullKeyPath)) throw new Error('newKey cannot be found');
+
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
+
+	var expectedBody = _expectedBody || ('Keys have been rotated!');
+	var expectedStatusCode = _expectedStatusCode || 200;
+
+	testClient.rotateKeys(serverSettings, fullKeyPath, function(res){
+		processRes(res, function(body){
+			assert.equal(res.statusCode, expectedStatusCode, 'Unexpected status code on key rotation: ' + res.statusCode);
+			assert.equal(body, expectedBody, 'Unexpected response body on key rotation: ' + body);
+			cb();
+		});
+	}, function(err){throw err;});
 
 };
 
@@ -126,14 +222,79 @@ exports.spoofedUsernameReq = function(withUsername){
 
 };
 
-exports.sessionAgreementReq = function(cb){
+exports.sessionAgreementReq = function(cb, wantedSessionExpiration, _expectedBody, _expectedStatusCode, _expectedSessionExpiration){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
+
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
+
+	var expectedBody = _expectedBody || ('Session created');
+	var expectedStatusCode = _expectedStatusCode || 200;
+	var expectedSessionExpiration = _expectedSessionExpiration || wantedSessionExpiration || 0; //Server-imposed || user-defined || 0 (default, no TTL on session)
+
+	var newSessionId = crypto.randomBytes(16);
+	testClient.createSession(serverSettings, newSessionId, wantedSessionExpiration, function(res){
+		processRes(res, function(body){
+			assert.equal(res.statusCode, expectedStatusCode, 'Unexpected status code on sessionId agreement: ' + res.statusCode);
+			//Check the session expiration, in addition to the status code and response body
+			var currentSessionExpiration = res.headers['hpka-session-expiration'];
+			if (expectedSessionExpiration != 0){
+				var upperExpirationWindow = expectedSessionExpiration + 5,
+					lowerExpirationWindow = expectedSessionExpiration - 5;
+				assert(currentSessionExpiration >= lowerExpirationWindow && currentSessionExpiration <= upperExpirationWindow, 'Unexpected session expiration: ' + currentSessionExpiration + '; expected session expiration: ' + expectedSessionExpiration);
+			} else {
+				assert(currentSessionExpiration == 0, 'Unexpected non-null session expiration: ' + currentSessionExpiration);
+			}
+			assert.equal(body, expectedBody, 'Unexpected response body: ' + body);
+			cb();
+		});
+	}, function(err){throw err;});
 
 };
 
-exports.sessionRevocationReq = function(cb){
+exports.sessionRevocationReq = function(cb, _expectedBody, _expectedStatusCode){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
+
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
 
 };
 
-exports.sessionAuthenticatedReq = function(cb){
+exports.sessionAuthenticatedReq = function(cb, _expectedBody, _expectedStatusCode){
+	if (typeof cb != 'function') throw new TypeError('cb must be a function');
 
+	if (_expectedBody && !isString(_expectedBody)) throw new TypeError('when defined, _expectedBody must be a non-null string');
+	validStatusCode(_expectedStatusCode);
+
+	testClient.request(serverSettings, function(res){
+		processRes(res, function(body){
+			
+		});
+	}, function(err){throw err;});
 };
+
+exports.getUserSessions = function(){
+	var s;
+	try {
+		s = testClient.getSessions();
+	} catch (e){
+		console.error(e.message);
+		return;
+	}
+	return s;
+};
+
+exports.getUserSessionsReference = function(){
+	var r;
+	try {
+		r = testClient.getSessionsReference();
+	} catch (e){
+		console.error(e.message);
+		return;
+	}
+	return r;
+};
+
+exports.setUserSessions = function(_s, merge){
+	testClient.setSessions(_s, merge);
+}
