97bb18cef485b52b881521e1e27b1dc034e7ff3d
Ahmad Ben Mrad <batikhsouri@gmail.com>
2016-02-14 13:33:49 +0100

    Getting rid of the public key utility functions' code from the test server

--- test/testServer.js
+++ test/testServer.js
@@ -26,58 +26,6 @@ function log(m){
 * Useful function for the server
 */
 
-//Getting the PKA info from a HPKAReq object
-function getPubKeyObject(HPKAReq){
-	//Checking that HPKAReq object is correctly formed
-	var reqObj = {};
-	if (!HPKAReq.keyType) throw new TypeError('Invalid HPKAReq obejct on getPubKeyObject method');
-	reqObj.keyType = HPKAReq.keyType;
-	if (HPKAReq.keyType == 'ecdsa'){ //ECDSA case
-		if (!(HPKAReq.curveName && HPKAReq.point && HPKAReq.point.x && HPKAReq.point.y)) throw new TypeError('Malformed ECDSA request');
-		reqObj.curveName = HPKAReq.curveName;
-		reqObj.point = HPKAReq.point;
-	} else if (HPKAReq.keyType == 'rsa'){ //RSA case
-		if (!(HPKAReq.modulus && HPKAReq.publicExponent)) throw new TypeError('Malformed RSA request');
-		reqObj.modulus = HPKAReq.modulus;
-		reqObj.publicExponent = HPKAReq.publicExponent;
-	} else if (HPKAReq.keyType == 'dsa'){ //DSA case
-		if (!(HPKAReq.primeField && HPKAReq.divider && HPKAReq.base && HPKAReq.publicElement)) throw new TypeError('Malformed DSA request');
-		reqObj.primeField = HPKAReq.primeField;
-		reqObj.divider = HPKAReq.divider;
-		reqObj.base = HPKAReq.base;
-		reqObj.publicElement = HPKAReq.publicElement;
-	} else if (HPKAReq.keyType == 'ed25519'){
-		if (!(HPKAReq.publicKey)) throw new TypeError('Malformed Ed25519 request');
-		reqObj.publicKey = HPKAReq.publicKey;
-	} else throw new TypeError('Invalid key type : ' + HPKAReq.keyType);
-	return reqObj;
-}
-
-function checkPubKeyObjects(pubKey1, pubKey2){
-	if (!(typeof pubKey1 == 'object' && typeof pubKey2 == 'object')) throw new TypeError('Parameters must be objects');
-	if (pubKey1.keyType != pubKey2.keyType) return false;
-	if (pubKey1.keyType == "ecdsa"){
-		//console.log('Common type : ecdsa');
-		if (pubKey1.curveName != pubKey2.curveName) return false;
-		if (pubKey1.point.x != pubKey2.point.x) return false;
-		if (pubKey1.point.y != pubKey2.point.y) return false;
-	} else if (pubKey1.keyType == "rsa"){
-		//console.log('Common type : rsa');
-		if (pubKey1.modulus != pubKey2.modulus) return false;
-		if (pubKey1.publicExponent != pubKey2.publicExponent) return false;
-	} else if (pubKey1.keyType == "dsa"){
-		//console.log('Common type : dsa');
-		if (pubKey1.primeField != pubKey2.primeField) return false;
-		if (pubKey1.divider != pubKey2.divider) return false;
-		if (pubKey1.base != pubKey2.base) return false;
-		if (pubKey1.publicElement != pubKey2.publicElement) return false;
-	} else if (pubKey1.keyType == 'ed25519'){
-		//console.log('Common type : ed25519');
-		if (pubKey1.publicKey != pubKey2.publicKey) return false;
-	} else throw new TypeError('Invalid keyType');
-	return true;
-}
-
 function writeRes(res, body, headers, statusCode){
 	headers = headers || {};
 	var bodyLength;
@@ -151,21 +99,20 @@ var postHandler = function(req, res){
 };
 
 var loginCheck = function(HPKAReq, req, res, callback){
-	if (userList[HPKAReq.username] && typeof userList[HPKAReq.username] == 'object' && checkPubKeyObjects(getPubKeyObject(HPKAReq), userList[HPKAReq.username])) callback(true);
+	if (userList[HPKAReq.username] && typeof userList[HPKAReq.username] == 'object' && HPKAReq.checkPublicKeyEqualityWith(userList[HPKAReq.username])) callback(true);
 	else callback(false);
 };
 
 var registration = function(HPKAReq, req, res){
 	var username = HPKAReq.username;
-	var keyInfo = getPubKeyObject(HPKAReq);
+	var keyInfo = HPKAReq.getPublicKey();
 	userList[username] = keyInfo;
 	writeRes(res, 'Welcome ' + username + ' !', {'Content-Type': 'text/plain'});
 };
 
 var deletion = function(HPKAReq, req, res){
 	if (typeof userList[HPKAReq.username] != 'object') return;
-	var keyInfo = getPubKeyObject(HPKAReq);
-	if (!checkPubKeyObjects(keyInfo, userList[HPKAReq.username])){
+	if (!HPKAReq.checkPublicKeyEqualityWith(userList[HPKAReq.username])){
 		writeHpkaErr(res, 'Invalid public key', 3);
 		return;
 	}
@@ -182,9 +129,9 @@ var keyRotation = function(HPKAReq, newKeyReq, req, res){
 		writeHpkaErr(res, 'Unregistered user', 4);
 	} else {
 		//Check that the actual key is correct
-		if (checkPubKeyObjects(userList[HPKAReq.username], getPubKeyObject(HPKAReq))){
-			//Replace the actual ke by the new key
-			userList[HPKAReq.username] = getPubKeyObject(newKeyReq);
+		if (HPKAReq.checkPublicKeyEqualityWith(userList[HPKAReq.username])){
+			//Replace the actual key by the new key
+			userList[HPKAReq.username] = newKeyReq.getPublicKey();
 			body = 'Keys have been rotated!';
 		} else {
 			body = 'Invalid public key'
@@ -217,7 +164,6 @@ var sessionCheck = function(SessionReq, req, res, callback){
 var sessionAgreement = function(HPKAReq, req, callback){
 	var username = HPKAReq.username;
 	var sessionId = HPKAReq.sessionId;
-	var keyInfo = getPubKeyObject(HPKAReq);
 	//Expiration date agreement
 	var finalSessionExpiration;
 	var n = Math.floor(Date.now() / 1000);
@@ -232,7 +178,7 @@ var sessionAgreement = function(HPKAReq, req, callback){
 		finalSessionExpiration = userSetExpiration;
 	}
 	//Check keys
-	if (typeof keyInfo == 'object' && checkPubKeyObjects(keyInfo, userList[username])){
+	if (HPKAReq.checkPublicKeyEqualityWith(userList[username])){
 		//Accept sessionId
 		if (sessions[username]){
 			//Save the sessionId in the existing array
@@ -249,9 +195,8 @@ var sessionAgreement = function(HPKAReq, req, callback){
 var sessionRevocation = function(HPKAReq, req, callback){
 	var username = HPKAReq.username;
 	var sessionId = HPKAReq.sessionId;
-	var keyInfo = getPubKeyObject(HPKAReq);
 	//Checks keys
-	if (typeof keyInfo == 'object' && checkPubKeyObjects(keyInfo, userList[username])){
+	if (HPKAReq.checkPublicKeyEqualityWith(userList[username])){
 		//Revoke sessionId
 		var currentSessionList = sessions[username];
 		if (currentSessionList){
